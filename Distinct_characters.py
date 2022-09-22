08d3b4180973886e28302b26b016dd7eeb8b9e77
mahalteja <106660291+mahalteja@users.noreply.github.com>
2022-09-22 20:58:03 +0530

    a=list(input().lower()) m=[] for i in a:     if a.count(i)==1 and i.lower() not in m and i!=' ':         m.append(i.lower()) m.sort() print(''.join(m))

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -1,7 +1,7 @@
-a=list(input())
+a=list(input().lower())
 m=[]
 for i in a:
-    if i.lower() not in m and i!=' ':
+    if a.count(i)==1 and i.lower() not in m and i!=' ':
         m.append(i.lower())
 m.sort()
 print(''.join(m))
