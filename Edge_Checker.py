4b08f6b10eee10fad8ce959543c065504ac57302
mahalteja <106660291+mahalteja@users.noreply.github.com>
2022-10-29 15:41:29 +0530

    n,m=map(int,input().split()) if n==1 and m==10 or n==10 and m==1:     print('Yes') elif m==n+1 or n==m+1:     print('Yes') else:     print('No')

--- Edge_Checker.py
+++ Edge_Checker.py
@@ -1,8 +1,7 @@
 n,m=map(int,input().split())
-if n==1 and m==10 or n==10 and m==1:
+if n==1 and m==10 or n==10 and m==1: 
     print('Yes')
-elif m==n+1 or n==m+1:
+elif m==n+1 or n==m+1: 
     print('Yes')
-else:
+else: 
     print('No')
-    
