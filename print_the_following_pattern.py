cfebb8b1f4ddef757b7596da637eea881e90966d
mahalteja <106660291+mahalteja@users.noreply.github.com>
2022-11-07 14:18:49 +0530

    n=int(input()) l=[i for i in range(n,0,-1)] for k in range(n):     for i in l:         print(i,end=' ')     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,5 +1,6 @@
 n=int(input())
-l=[str(i) for i in range(1,n+1)]
-for i in range(n):
-    print(''.join(l))
-    l.pop()
+l=[i for i in range(n,0,-1)]
+for k in range(n):
+    for i in l:
+        print(i,end=' ')
+    print()    
